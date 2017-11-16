2efead0810960eb13519eaa4677764b05635a25d
Daniel Albuquerque <daniel.alb.querque@gmail.com>
2017-11-16 06:45:15 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header } from './src/components/common';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header headerText="Tech Stack" />
+        </View>
+      </Provider>
+    );
+  }
+}
